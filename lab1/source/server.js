e9f7e25045950ba1990cc21d41e05b7cd4907259
macszl <235989@edu.p.lodz.pl>
2023-03-06 15:56:01 +0100

    added lines to server.js

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let sentText = "";
+    
+    if(req.query.audioPlayer)
+    {
+      sentText += `<audio id=audioPlayer src=` + res.query.audioPlayer + `> </audio>`
+    }
+
+    if(req.query.videoPlayer)
+    {
+      sentText += `<video id=videoPlayer src=` + res.query.videoPlayer + `> </video>`
+    }
+    res.send(sentText);
 })
 
 app.listen(4080)
