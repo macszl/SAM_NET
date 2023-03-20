7a7a38c58643637068802c84f46b8d1a264a3aa0
macszl <235989@edu.p.lodz.pl>
2023-03-20 14:33:18 +0100

    added script tag

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -2,22 +2,35 @@ const express = require('express')
 
 const app = express()
 
+
 app.get('/', (req, res) => {
     let sentText = "";
     
     if(req.query.audioPlayer)
     {
-      sentText += `<audio id=audioPlayer src=` + res.query.audioPlayer + `> </audio>`
+      sentText += `<audio id="audioPlayer" src=` + res.query.audioPlayer + `> </audio>`
     }
 
     if(req.query.videoPlayer)
     {
-      sentText += `<video id=videoPlayer src=` + res.query.videoPlayer + `> </video>`
+      sentText += `<video id="videoPlayer" src=` + res.query.videoPlayer + `> </video>`
     }
 
-    let videoCancelButton = `<button id=videoCancel onclick= ></button>`
-    let audioCancelButton = `<button id=audioCancel onclick= ></button>`
-    res.send(sentText);
+    if(req.query.imgFile)
+    {
+      sentText += `<img id="posterImage" src=` + res.query.imgFile+  `></img>`
+    }
+
+    let videoCancelButton = `<button id="videoCancel" onclick=changeOnSrc("videoPlayer", "cancel.mp4") > Cancel video </button>`;
+    let audioCancelButton = `<button id="audioCancel" onclick=changeOnSrc("audioPlayer", "cancel.mp3") > Cancel audio </button>`;
+    let scriptTag = `<script>
+      function changeOnSrc(id, onClickValue)
+      {
+        element = Document.getElementById(id);
+        element.src = onClickValue; 
+      }
+    </script>`
+    res.send(sentText + videoCancelButton + audioCancelButton + scriptTag);
 })
 
 app.listen(4080)
